3263580be29f674e0d99396b8b1214160a1edc9c
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2023-05-30 20:50:29 +0900

    update 변수, 메서드, 메모리 구조 개념 + 매개변수 소스 추가

---
+++ ch06/src/ParameterTest1.java
@@ -0,0 +1,18 @@
+public class ParameterTest1 {
+
+	public static void main(String[] args) {
+		// 기본형 매개변수는 heap메모리의 인스턴스에 영향을 줄 수 없다.
+		Data d = new Data();
+		d.x = 10;
+		
+		System.out.println("���� �� d.x��: " + d.x);
+		
+		change(d.x);
+		System.out.println("���� �� d.x��: " + d.x);
+	}
+
+	static void change(int x) {
+		x = 1000;
+		System.out.println("x ��: " + x);
+	}
+}
---
+++ ch06/src/ParameterTest2.java
@@ -0,0 +1,18 @@
+public class ParameterTest2 {
+
+	public static void main(String[] args) {
+		// 참조형 매개변수는 heap메모리의 인스턴스에 영향을 준다.
+		Data d = new Data();
+		d.x = 10;
+		
+		System.out.println("���� �� d.x��: " + d.x);
+		
+		change(d);
+		System.out.println("���� �� d.x��: " + d.x);
+	}
+
+	static void change(Data d) {
+		d.x = 1000;
+		System.out.println("d.x ��: " + d.x);
+	}
+}
